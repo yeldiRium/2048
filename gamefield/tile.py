22608e736d84bdc3096be20fef5309b628dd4920
Hannes Leutloff <hannes.leutloff@aol.de>
2016-04-05 23:46:20 +0200

    implement can_move_to and can_be_replaced_with on EmptyTile and Blockingtile

--- gamefield/tile.py
+++ gamefield/tile.py
@@ -2,12 +2,33 @@ from abc import ABCMeta, abstractmethod
 
 
 class Tile(metaclass=ABCMeta):
-    pass
+    @abstractmethod
+    def can_move_to(self, tile: 'Tile') -> bool:
+        """
+        True, if self can move onto the given tile.
+        Often calls the given tile for can_be_replaced_with.
+        """
+        pass
+
+    @abstractmethod
+    def can_be_replaced_with(self, tile: 'Tile') -> bool:
+        """
+        True, if the given tile can be moved onto self.
+        """
+        pass
 
 
 class EmptyTile(Tile):
-    pass
+    def can_move_to(self, tile: 'Tile') -> bool:
+        return False
+
+    def can_be_replaced_with(self, tile: 'Tile') -> bool:
+        return True
 
 
 class BlockingTile(Tile):
-    pass
+    def can_move_to(self, tile: 'Tile') -> bool:
+        return tile.can_be_replaced_with(self)
+
+    def can_be_replaced_with(self, tile: 'Tile') -> bool:
+        return False
