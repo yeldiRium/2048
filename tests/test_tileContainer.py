19060e9f0c62c67b7b31448feedba75ff45e0604
Hannes Leutloff <hannes.leutloff@aol.de>
2016-04-05 22:43:35 +0200

    add asserts for TileContainers containing BlockingTiles to TileContainerTestCase

--- tests/test_tileContainer.py
+++ tests/test_tileContainer.py
@@ -8,6 +8,7 @@ from gamefield.tilecontainer import TileContainer
 class TileContainerTestCase(unittest.TestCase):
     def setUp(self):
         self.empty_tile_container = TileContainer(EmptyTile())
+        self.blocking_tile_container = TileContainer(BlockingTile())
         # TODO: add other tiles to containers
 
     def test_tileProperty(self):
@@ -16,5 +17,8 @@ class TileContainerTestCase(unittest.TestCase):
         turn, then it returns a BlockingTile.
         """
         self.assertIsInstance(self.empty_tile_container.tile, EmptyTile)
+        self.assertIsInstance(self.blocking_tile_container.tile, BlockingTile)
         self.empty_tile_container.fused = True
+        self.blocking_tile_container.fused = True
         self.assertIsInstance(self.empty_tile_container.tile, BlockingTile)
+        self.assertIsInstance(self.blocking_tile_container.tile, BlockingTile)
