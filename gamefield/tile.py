f0f30628762e27dbb39589418dd0914e7d9894b1
Hannes Leutloff <hannes.leutloff@aol.de>
2016-04-05 23:54:08 +0200

    update documentation for Tiles

--- gamefield/tile.py
+++ gamefield/tile.py
@@ -20,15 +20,31 @@ class Tile(metaclass=ABCMeta):
 
 class EmptyTile(Tile):
     def can_move_to(self, tile: 'Tile') -> bool:
+        """
+        Since EmptryTiles don't represent anything other than nothing, it is
+        wasted time to move them. So they don't let themselves get moved.
+        """
         return False
 
     def can_be_replaced_with(self, tile: 'Tile') -> bool:
+        """
+        Since EmptyTiles are supposed to be empty, everything can move onto
+        them.
+        """
         return True
 
 
 class BlockingTile(Tile):
     def can_move_to(self, tile: 'Tile') -> bool:
+        """
+        BlockingTiles are simple tiles that underly the usual physical rules and
+        can thus be moved onto everything that want to be moved on.
+        """
         return tile.can_be_replaced_with(self)
 
     def can_be_replaced_with(self, tile: 'Tile') -> bool:
+        """
+        Are simple tiles that take up space and thus other tiles can't be moved
+        onto them.
+        """
         return False
