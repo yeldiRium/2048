2041125eda43625ba3944d8941ad7d2ab7e1a1c4
Hannes Leutloff <hannes.leutloff@aol.de>
2016-04-05 22:13:30 +0200

    add Tests for TileContainer

---
+++ tests/test_tileContainer.py
@@ -0,0 +1,20 @@
+import unittest
+
+from gamefield.tile import EmptyTile
+from gamefield.tile import BlockingTile
+from gamefield.tilecontainer import TileContainer
+
+
+class TileContainerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.empty_tile_container = TileContainer(EmptyTile)
+        # TODO: add other tiles to containers
+
+    def test_tileProperty(self):
+        """
+        Asserts that the TileContainer returns its Tile unless it was fused this
+        turn, then it returns a BlockingTile.
+        """
+        self.assertIsInstance(self.empty_tile_container.tile, EmptyTile)
+        self.empty_tile_container.fused = True
+        self.assertIsInstance(self.empty_tile_container.tile, BlockingTile)
