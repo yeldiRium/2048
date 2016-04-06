94391c2402bb8e5deec5e1e9eab26bc3a4cf1346
Hannes Leutloff <hannes.leutloff@aol.de>
2016-04-06 02:50:12 +0200

    add tests for ValueTile

--- tests/test_tile.py
+++ tests/test_tile.py
@@ -6,49 +6,91 @@ from gamefield.tilecollection import TileCollection
 
 class EmptyTileTestCase(unittest.TestCase):
     def setUp(self):
-        # TODO: add ValueTile
         self.tile_collection = TileCollection()
         self.test_tile = self.tile_collection.get_tile('empty')
         self.empty_tile = self.test_tile
         self.blocking_tile = self.tile_collection.get_tile('blocking')
+        self.value_tile = self.tile_collection.get_tile('value', value=4)
 
     def test_movingToEmptyTile(self):
         self.assertTrue(self.test_tile.can_be_replaced_with(self.empty_tile))
         self.assertTrue(self.test_tile.can_be_replaced_with(self.blocking_tile))
+        self.assertTrue(self.test_tile.can_be_replaced_with(self.value_tile))
 
     def test_movingEmptyTile(self):
         self.assertFalse(self.test_tile.can_move_to(self.empty_tile))
         self.assertFalse(self.test_tile.can_move_to(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_move_to(self.value_tile))
 
     def test_fusingWithEmptyTile(self):
         self.assertFalse(self.test_tile.can_fuse_with(self.empty_tile))
         self.assertFalse(self.test_tile.can_fuse_with(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_fuse_with(self.value_tile))
 
     def test_fusingEmptyTile(self):
         self.assertFalse(self.test_tile.can_accept_fusion_with(self.empty_tile))
         self.assertFalse(self.test_tile.can_accept_fusion_with(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_accept_fusion_with(self.value_tile))
 
 
 class BlockingTileTestCase(unittest.TestCase):
     def setUp(self):
-        # TODO : add ValueTile
         self.tile_collection = TileCollection()
         self.test_tile = self.tile_collection.get_tile('blocking')
         self.empty_tile = self.tile_collection.get_tile('empty')
         self.blocking_tile = self.test_tile
+        self.value_tile = self.tile_collection.get_tile('value', value=4)
 
     def test_movingToBlockingTile(self):
         self.assertFalse(self.test_tile.can_be_replaced_with(self.empty_tile))
         self.assertFalse(self.test_tile.can_be_replaced_with(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_be_replaced_with(self.value_tile))
 
-    def test_movingblockingTile(self):
+    def test_movingBlockingTile(self):
         self.assertTrue(self.test_tile.can_move_to(self.empty_tile))
         self.assertFalse(self.test_tile.can_move_to(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_move_to(self.value_tile))
 
     def test_fusingWithBlockingTile(self):
         self.assertFalse(self.test_tile.can_fuse_with(self.empty_tile))
         self.assertFalse(self.test_tile.can_fuse_with(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_fuse_with(self.value_tile))
 
     def test_fusingBlockingTile(self):
         self.assertFalse(self.test_tile.can_accept_fusion_with(self.empty_tile))
         self.assertFalse(self.test_tile.can_accept_fusion_with(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_accept_fusion_with(self.value_tile))
+
+
+class ValueTileTestCase(unittest.TestCase):
+    def setUp(self):
+        self.tile_collection = TileCollection()
+        self.test_tile = self.tile_collection.get_tile('value', value=2)
+        self.empty_tile = self.tile_collection.get_tile('empty')
+        self.blocking_tile = self.tile_collection.get_tile('blocking')
+        self.fitting_value_tile = self.test_tile
+        self.unfitting_value_tile = self.tile_collection.get_tile('value', value=4)
+
+    def test_movingToValueTile(self):
+        self.assertFalse(self.test_tile.can_be_replaced_with(self.empty_tile))
+        self.assertFalse(self.test_tile.can_be_replaced_with(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_be_replaced_with(self.fitting_value_tile))
+        self.assertFalse(self.test_tile.can_be_replaced_with(self.unfitting_value_tile))
+
+    def test_movingValueTile(self):
+        self.assertTrue(self.test_tile.can_move_to(self.empty_tile))
+        self.assertFalse(self.test_tile.can_move_to(self.blocking_tile))
+        self.assertFalse(self.test_tile.can_move_to(self.fitting_value_tile))
+        self.assertFalse(self.test_tile.can_move_to(self.unfitting_value_tile))
+
+    def test_fusingWithValueTile(self):
+        self.assertFalse(self.test_tile.can_fuse_with(self.empty_tile))
+        self.assertFalse(self.test_tile.can_fuse_with(self.blocking_tile))
+        self.assertTrue(self.test_tile.can_fuse_with(self.fitting_value_tile))
+        self.assertFalse(self.test_tile.can_fuse_with(self.unfitting_value_tile))
+
+    def test_fusingValueTile(self):
+        self.assertFalse(self.test_tile.can_accept_fusion_with(self.empty_tile))
+        self.assertFalse(self.test_tile.can_accept_fusion_with(self.blocking_tile))
+        self.assertTrue(self.test_tile.can_accept_fusion_with(self.fitting_value_tile))
+        self.assertFalse(self.test_tile.can_accept_fusion_with(self.unfitting_value_tile))
