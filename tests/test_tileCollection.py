5681f6c6c296b1280a408752f766a31f4e1b7c31
Hannes Leutloff <hannes.leutloff@aol.de>
2016-04-06 12:51:38 +0200

    add tests for TileCollection.fuse()

--- tests/test_tileCollection.py
+++ tests/test_tileCollection.py
@@ -1,6 +1,6 @@
 import unittest
 
-from gamefield.tile import EmptyTile, BlockingTile
+from gamefield.tile import EmptyTile, BlockingTile, ValueTile
 from gamefield.tilecollection import TileCollection
 
 
@@ -49,3 +49,31 @@ class TileCollectionTestCase(unittest.TestCase):
     def test_invalidTypeRaisesException(self):
         with self.assertRaises(Exception):
             self.tile_collection.get_tile('invalid_tilename')
+
+    def test_fuse(self):
+        """
+        Tests that fusing two value tiles returns a value tile with their sum as
+        its value.
+        """
+        self.assertEqual(
+            self.tile_collection.get_tile('value',  value=32),
+            self.tile_collection.fuse(
+                self.tile_collection.get_tile('value',  value=16),
+                self.tile_collection.get_tile('value',  value=16)
+            )
+        )
+        # this is technically illegal in the game, but works for engine purpo-
+        # ses:
+        self.assertEqual(
+            self.tile_collection.get_tile('value',  value=12),
+            self.tile_collection.fuse(
+                self.tile_collection.get_tile('value',  value=4),
+                self.tile_collection.get_tile('value',  value=8)
+            )
+        )
+
+        with self.assertRaises(Exception):
+            self.tile_collection.fuse(
+                self.tile_collection.get_tile('empty'),
+                self.tile_collection.get_tile('value',  value=8)
+            )
